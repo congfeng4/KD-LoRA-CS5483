af641b5e7721e4919d3875807323c72ee2896256
congfeng <cgsdfc@126.com>
2026-01-23 21:52:18 +0800

    Add peft back

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 conda activate lora
 export CUDA_VISIBLE_DEVICES=2
-python src/BERT.py --task wnli --num_train_epochs 3
-# python src/BERT_Distill_LoRA.py
+#python src/BERT.py --task wnli --num_train_epochs 3
+python src/BERT_Distill_LoRA.py
