1405f492f28b88955c80e69ac18ff36d1a9427a8
congfeng <cgsdfc@126.com>
2026-01-24 15:38:13 +0800

    Save from server

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@ conda activate lora
 # export CUDA_VISIBLE_DEVICES=2
 #python src/BERT.py --task wnli --num_train_epochs 3
 accelerate launch src/BERT_Distill_LoRA.py -t 0
-#accelerate launch src/BERT_Distill_LoRA.py -t 1
+# accelerate launch src/BERT_Distill_LoRA.py -t 1
 #accelerate launch src/BERT_Distill_LoRA.py -t 2
-
